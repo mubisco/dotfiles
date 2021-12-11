753b8e40e932a938f1226b347f72d300fc57ccae
Xan Bellón <xan.bellon@gmail.com>
2021-12-11 15:55:10 +0100

    Restarting autoconfig script

--- autoconfig.sh
+++ autoconfig.sh
@@ -11,30 +11,33 @@ else
         exit 1
     fi
 fi
-echo "Installing minimun deps..."
-# sudo pacman -Sy --noconfirm zsh lsd tmux composer nodejs npm python-pip fzf powerline powerline-fonts ctags zsh-theme-powerlevel9k wget
-# sudo npm install -g neovim
+#echo "Installing minimun deps..."
+#sudo pacman -Sy --noconfirm zsh lsd tmux composer nodejs npm python-pip fzf powerline powerline-fonts ctags zsh-theme-powerlevel9k wget
+#sudo npm install -g neovim
 
-su $WHOAMI
-echo "Copying files..."
-cp -rvf ./config ~/.config/
+#su $WHOAMI
+# ----- USER SECTION -----
+#echo "Copying files..."
+#cp -rvf ./config ~/.config/
 
-echo "Setting up neovim..."
-# python3 -m pip install --user --upgrade pynvim
-# nvim +'PlugInstall --sync' +qa
+#echo "Setting up neovim..."
+#python3 -m pip install --user --upgrade pynvim
+#nvim +'PlugInstall --sync' +qa
+#nvim +'CocInstall coc-json coc-css coc-docker coc-eslint coc-gitignore coc-html coc-json coc-marketplace coc-phpls cocsh coc-stylelint coc-tsserver coc-ultisnips coc-vetur coc-webpack coc-yaml' +qall
+# TODO: coc-python
 # npm install --prefix ~/.config/coc/extensions
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo "Installing fonts..."
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip
-mkdir ~/.local/share/fonts
-unzip FantasqueSansMono.zip -d ~/.local/share/fonts
-rm -f FantasqueSansMono.zip
-fc-cache
+#echo "Installing fonts..."
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip
+#mkdir ~/.local/share/fonts
+#unzip FantasqueSansMono.zip -d ~/.local/share/fonts
+#rm -f FantasqueSansMono.zip
+#fc-cache
 
-cp zshrc ~/.zshrc
-cp gitconfig ~/.gitconfig
-chsh /usr/bin/zsh
+#cp zshrc ~/.zshrc
+#cp gitconfig ~/.gitconfig
+#chsh /usr/bin/zsh
