3649095660e78f7ee1433e38eb2664c23d0bc350
Xan Bellón <xan.bellon@gmail.com>
2021-12-18 16:39:00 +0100

    Removed zsh config overwrite
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- autoconfig.sh
+++ autoconfig.sh
@@ -15,7 +15,7 @@ echo 'Server = https://repo.archlinuxcn.org/$arch' >> /etc/pacman.conf
 
 echo "Instaling minimun deps..."
 sudo pacman -Sy --noconfirm zsh composer sudo lsd tmux composer nodejs npm python-pip fzf powerline powerline-fonts \
-  ctags zsh-theme-powerlevel10k zsh-autosuggestions zsh-syntax-highlighting wget nerd-fonts \
+  ctags wget curl nerd-fonts \
   binutils make gcc pkg-config fakeroot
 sudo npm install -g neovim
 
@@ -44,20 +44,13 @@ cd /home/mubisco
 echo "Setting up oh-my-zsh..."
 su mubisco -c 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
 
-su mubisco -c "cp -f /home/mubisco/Projects/dotfiles/zshrc /home/mubisco/.zshrc"
+#su mubisco -c "cp -f /home/mubisco/Projects/dotfiles/zshrc /home/mubisco/.zshrc"
 su mubisco -c "cp -f /home/mubisco/Projects/dotfiles/gitconfig /home/mubisco/.gitconfig"
 su mubisco -c "composer global require squizlabs/php_codesniffer"
 
 echo "Setting up neovim..."
-su mubisco -c "python3 -m pip install --user --upgrade pynvim"
-su mubisco -c "nvim +'PlugInstall --sync' +qa"
-su mubisco -c "mkdir -p /home/mubisco/.config/coc/extensions"
-cd /home/mubisco/.config/coc/extensions
-if [ ! -f package.json ]
-then
-    su mubisco -c "echo '{\"dependencies\":{}}'> package.json"
-fi
-su mubisco -c "npm install coc-json coc-css coc-docker coc-eslint coc-gitignore coc-html coc-json coc-marketplace coc-phpls coc-sh coc-stylelint coc-tsserver coc-ultisnips coc-vetur coc-webpack coc-yaml coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod"
+su mubisco -c "python3 -m pip install --user --upgrade pynvim jedi dbgp pylint pep8 jedi-language-server flake8 mypy"
 
+su mubisco -c "nvim +'PlugInstall --sync' +qa"
 
 #chsh /usr/bin/zsh
