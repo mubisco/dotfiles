f4799c0bb3c8a5d874c0ac489c66b7faee3407ad
Xan Bellón <xan.bellon@gmail.com>
2022-06-05 15:16:27 +0200

    Added ohMyZsh aliases
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

---
+++ oh-my-zsh/custom/001-mubisco.zsh
@@ -0,0 +1,17 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+alias zshconfig="nvim ~/.zshrc"
+alias vimconfig="nvim ~/.config/nvim/init.vim"
+alias cfgreload="source ~/.zshrc"
+alias ll="lsd -l"
+alias ls="lsd"
+alias la="lsd -la"
+alias v="nvim"
---
+++ oh-my-zsh/custom/002-symfony.zsh
@@ -0,0 +1,15 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+alias sf="symfony"
+alias sfc="symfony console"
+alias scc="symfony console cache:clear"
+alias sft="php bin/phpunit"
+alias sftu="php bin/phpunit tests/unit"
