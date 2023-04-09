38ab518e0e00f4e86176826f04b2fcedebb59f36
Xan Bellón <xan.bellon@gmail.com>
2023-04-09 16:00:39 +0200

    Updated qtile bars and shortcuts

--- config/qtile/settings/widgets.py
+++ config/qtile/settings/widgets.py
@@ -70,16 +70,11 @@ primary_widgets = [
 
     powerline('color4', 'dark'),
 
-    icon(bg="color4", text=' '), # Icon: nf-fa-download
+    icon(bg="color4", text=' '), # Icon: nf-fa-volume_off
 
-    widget.CheckUpdates(
-        background=colors['color4'],
-        colour_have_updates=colors['text'],
-        colour_no_updates=colors['text'],
-        no_update_string='0',
-        display_format='{updates}',
-        update_interval=1800,
-        custom_command='checkupdates',
+    widget.PulseVolume(
+        **base(bg='color4'),
+        fmt='Vol: {}'
     ),
 
     powerline('color3', 'color4'),
