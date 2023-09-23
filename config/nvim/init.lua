d97ab8f40a1bf4bd72e92163e6f3cb1cf5460335
Xan Bellón <xan.bellon@gmail.com>
2023-09-23 21:32:34 +0200

    refactor: reorganized init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,9 @@
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
 require "user.options"
 require "user.keymaps"
+require "user.nvim-tree"
 require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
@@ -9,13 +13,8 @@ require "user.gitsigns"
 require "user.treesitter"
 require "user.autopairs"
 require "user.comment"
-require "user.nvim-tree"
--- require "user.bufferline"
 require "user.lualine"
 require "user.toggleterm"
--- require "user.project"
--- require "user.impatient"
--- require "user.indentline"
 require "user.alpha"
 require "user.autocommands"
 require "user.uuid"
