e725a545efded4f66e46b9d91befa71cefabe1cb
Xan Bellón <xan@life5.com>
2024-03-05 13:02:11 +0100

    chore: monokai-pro setup
    Signed-off-by: Xan Bellón <xan@life5.com>

--- config/nvim/lua/user/theme-monokai-pro.lua
+++ config/nvim/lua/user/theme-monokai-pro.lua
@@ -12,7 +12,7 @@ require("monokai-pro").setup({
     annotation = { italic = true },
     tag_attribute = { italic = true }, -- attribute of tag in reactjs
   },
-  filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+  filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
   -- Enable this will disable filter option
   day_night = {
     enable = false, -- turn off by default
@@ -46,4 +46,5 @@ require("monokai-pro").setup({
 })
 
 vim.cmd([[colorscheme monokai-pro]])
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 -- colorscheme monokai-pro
