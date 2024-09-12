5f9152ad36064db257475955d4f06494518b16ca
Lucas Nunzi <lucas.nunzi@life5.com>
2024-09-13 00:47:57 +0200

    add mouse support

---
+++ config/nvim/lua/user/override/init.lua
@@ -0,0 +1,8 @@
+-- require 'user.override.user.plugins'
+require 'user.override.user.alpha'
+require 'user.override.user.harpoon'
+require 'user.override.user.luasnip'
+require 'user.override.user.keymap'
+require 'user.override.user.autocommands'
+require 'user.override.user.filetypes'
+vim.opt.mouse='a'
