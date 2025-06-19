31d55bfb6ad73afac7cf64bf5c7379e3e5ecd086
Xan Bellon <xan.bellon@exoticca.com>
2025-06-19 14:48:10 +0200

    Restore alpha config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,20 +12,14 @@ vim.g.loaded_netrwPlugin = 1
 require "user.options"
 require "user.keymaps"
 require "user.lazy"
+require "user.alpha"
 require "user.theme-monokai-pro"
 require "user.cmp"
 require "user.lsp"
-require "user.telescope"
-require "user.gitsigns"
 require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.lualine"
 require "user.toggleterm"
-require "user.alpha"
 require "user.autocommands"
 require "user.dap"
-require "user.neotest"
 
 loadrequire("user.override.init")
 
