b3cbcee67da9e12172fd70145134adffc45814ca
Xan Bellón <xan.bellon@gmail.com>
2025-06-28 13:48:04 +0200

    Move treesitter config to plugins file

--- config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -1,4 +1,51 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
+    configs.setup({
+      ensure_installed = {
+        "bash",
+        "c",
+        "c_sharp",
+        "css",
+        "dockerfile",
+        "diff",
+        "go",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "python",
+        "rust",
+        "scss",
+        "sql",
+        "tsx",
+        "twig",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml"
+      },
+      ignore_install = { "phpdoc" },
+      highlight = {
+        enable = true,
+        disable = { "css" },
+      },
+      autopairs = {
+        enable = true,
+      },
+      indent = { enable = true, disable = { "python", "css" } },
+    })
+  end,
 }
