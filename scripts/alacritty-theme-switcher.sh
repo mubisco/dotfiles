92893549eccdb0e03c1ee829c842fe57b83b3d3b
Xan Bellón <xan.bellon@gmail.com>
2025-12-16 12:13:05 +0100

    Improve theme switching on alacritty

--- scripts/alacritty-theme-switcher.sh
+++ scripts/alacritty-theme-switcher.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 DOTFILES_DIR="$SCRIPT_DIR/.."
 THEMES_DIR="$DOTFILES_DIR/config/alacritty/themes/themes"
-ALACRITTY_CONFIG="$DOTFILES_DIR/config/alacritty/alacritty.toml"
+ALACRITTY_THEME_FILE="$DOTFILES_DIR/config/alacritty/theme.toml"
 
 THEMES=$(ls -1 "$THEMES_DIR"/*.toml | sed -e 's/\.toml$//' -e 's/.*\///' | sort)
 
@@ -13,10 +13,7 @@ if [ -n "$SELECTED_THEME" ]; then
     # Construct the full path for the import statement
     THEME_PATH="~/.config/alacritty/themes/themes/$SELECTED_THEME.toml"
 
-    # Use a temporary file for sed to avoid issues with some versions of sed
-    TMP_FILE=$(mktemp)
-    sed "s|^import = .*|import = [\"$THEME_PATH\"]|" "$ALACRITTY_CONFIG" > "$TMP_FILE" && mv "$TMP_FILE" "$ALACRITTY_CONFIG"
+    echo -e "[general]\nimport = [\"$THEME_PATH\"]" > "$ALACRITTY_THEME_FILE"
 
     echo "Alacritty theme changed to $SELECTED_THEME"
 fi
-
--- user-setup.d/50-alacritty-themes.sh
+++ user-setup.d/50-alacritty-themes.sh
@@ -8,10 +8,21 @@ fi
 USERNAME=$1
 DOTFILES_DIR="/home/$USERNAME/Projects/dotfiles"
 THEMES_DIR="$DOTFILES_DIR/config/alacritty/themes"
+ALACRITTY_CONFIG_DIR="$DOTFILES_DIR/config/alacritty"
+ALACRITTY_THEME_FILE="$ALACRITTY_CONFIG_DIR/theme.toml"
+
 
 if [ ! -d "$THEMES_DIR" ] || [ -z "$(ls -A "$THEMES_DIR")" ]; then
     echo "Cloning alacritty themes..."
     git clone https://github.com/alacritty/alacritty-theme.git "$THEMES_DIR"
 else
     echo "Alacritty themes already exist."
+fi
+
+# Create a default theme.toml if it doesn't exist
+if [ ! -f "$ALACRITTY_THEME_FILE" ]; then
+    echo "Creating default alacritty theme.toml..."
+    echo -e "[general]\nimport = [\"~/.config/alacritty/themes/themes/solarized-dark.toml\"]" > "$ALACRITTY_THEME_FILE"
+else
+    echo "Alacritty theme.toml already exists."
 fi
