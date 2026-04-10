48429b9d3817f41470e795f4a08ed0aed9bdcee1
Xan Bellón <xan.bellon@fynkus.com>
2026-04-10 08:37:27 +0200

    Fix comment error message

--- config/nvim/lua/user/plugins/comment.lua
+++ config/nvim/lua/user/plugins/comment.lua
@@ -1,1 +1,8 @@
-return { "numToStr/Comment.nvim" }
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    pre_hook = function(ctx)
+      return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
+    end,
+  },
+}
